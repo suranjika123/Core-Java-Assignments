7f74bfa3c579d763d708d4352b093419bdd9c9bd
suranjika123 <patnaik.kajal24@gmail.com>
2018-03-02 22:20:53 -0600

    Data types example

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kajal
+ *
+ */
+public class DataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Primitive Data Types : The data types defined in the programming language
+		int age = 30; // Integer data type variable declaration and initialization
+		char sex = 'M'; // Character Data type
+		float salary = 2.5f; // Float data type
+		boolean job = true; // Bollean data type either true or false
+		String name = "Kajal"; // Java class used to declare a string variable
+		
+		// Non Primitive Data types defined by the programmer.
+		DataTypes dT = new DataTypes();
+		
+		System.out.println(name + " " + sex + " " + age + " " + job + " " + salary );
+	}
+
+}
